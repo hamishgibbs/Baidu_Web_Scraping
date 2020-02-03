79ba3262f3cd0377573b6187612b998a2941e88a
Hamish Gibbs <hamishgibbs@rosalinds-imac.lshtm.ac.uk>
2020-02-03 13:46:16 +0000

    manage chrome timeout

--- Scrape_2.py
+++ Scrape_2.py
@@ -10,8 +10,11 @@ from selenium import webdriver
 import time
 import bs4 as BeautifulSoup
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options  
 import pandas as pd
 import os
+import datetime
+from multiprocessing import Pool
 
 #%%
 '''
@@ -79,52 +82,146 @@ def scrape_panel_data(driver):
     return(panel_df)
 
 def scrape_line_graph(driver):
-    
-    #get line chart    
     line_chart = driver.find_element_by_xpath('//*[@id="content"]/div/div[6]')
 
     line_chart_data = []
-
     #chart data only appears one value at a time - hover over every item in the chart and get its value
     
     #hover over chart starting from left to 700 pixels R (steps of 20 px)
     #This offset is in relation to default chrome window - changing window/monitor size will result in incorrect data
-    for xoffset in range(0, 700, 15):
-        hover = ActionChains(driver).move_to_element_with_offset(line_chart, xoffset=xoffset, yoffset=100)
+    for xoffset in range(0, 700, 7):
+        hover = ActionChains(driver).move_to_element_with_offset(line_chart, xoffset=xoffset, yoffset=150)
         hover.perform()
-        very_short_wait()
+        time.sleep(0.001)
         
         #get the container holding the current data of the hovered point
         current_line_data_container = driver.find_element_by_xpath('//*[@id="content"]/div/div[6]/div[1]/div[2]')
-        
-        try:
-            #extract value and date from hovered point
-            line_data_soup = BeautifulSoup.BeautifulSoup(current_line_data_container.get_attribute('outerHTML'), 'html.parser')
-            line_value_soup = line_data_soup.find_all('div')
+    
+        #extract value and date from hovered point
+        line_data_soup = BeautifulSoup.BeautifulSoup(current_line_data_container.get_attribute('outerHTML'), 'html.parser')
+    
+        line_value_soup = line_data_soup.find_all('div')
+    
+        for line in line_value_soup:
             
-            for line in line_value_soup:
+            #try catch for any hovered points without data
+            try:
                 line = line.getText()
-                line_date = line[0:5]
-                line_value = line.split(': ')[1]
+                
+                text_location_2020 = line.find('今年迁徙规模指数: ')
+                text_location_2019 = line.find('去年迁徙规模指数: ')
+                       
+                #see if there is a 2020 data value:
+                if text_location_2020 > 0:
+                    
+                    #if there is a 2020 value: extract date, 2020 value and 2019 value
+                    line_date = line[: text_location_2020]
+                    data_2020 = line[text_location_2020:text_location_2019]
+                    data_2019 = line[text_location_2019:]
+                    data_2020_value = data_2020.split(': ')[1]
+                    data_2019_value = data_2019.split(': ')[1]
+                    
+                else:
+                    #if there is no 2020 value, extract date and 2019 value
+                    line_date = line[: text_location_2019]
+                    data_2019 = line[text_location_2019:]
+                    data_2020_value = 'NULL'
+                    data_2019_value = data_2019.split(': ')[1]
+            except:
+                continue
+    
+            line_chart_data.append({'date':line_date, '2019':data_2019_value, '2020':data_2020_value}) 
+    
+    #reconcile numeric dates with chinese dates (recorded)
+    td = (datetime.date.today() + datetime.timedelta(days=15)) - datetime.date(2020, 1, 1)
+    
+    line_date_labels = []
+    
+    #correctly format numeric dates - this will error with leap year?
+    for i in range(td.days + 1):
+        line_date_label = str(datetime.date(2020, 1, 1) + datetime.timedelta(days=i))
+        line_date_label = line_date_label[5:]
+        line_date_label = line_date_label.replace('-', '_')
+        line_date_labels.append(line_date_label)
+
+    #try to create dataframe from line date - if there is no data - return none
+    try:
+        line_df = pd.DataFrame(line_chart_data)
+        line_df.drop_duplicates(inplace=True)
+        line_df.reset_index(inplace=True)
+        
+        #correct an error where the last selected record becomes the first if the function used in succession 
+        if line_df.loc[0, '2020'] == 'NULL':
+            line_df = line_df.drop(0, axis=0)
+        
+        line_df['date'] = line_date_labels
             
-            line_chart_data.append({'date':line_date, 'value':line_value})    
-            
-        except:
-            continue
+        line_df.reset_index(inplace=True)
+        
+        line_df = line_df.filter(['date', '2019', '2020'])
+        
+        return(line_df)
+    except Exception as e: 
+        print(e)
+        return(None)
     
-    line_df = pd.DataFrame(line_chart_data)
-    line_df.drop_duplicates(inplace=True)
-    line_df.reset_index(inplace=True)
+def scrape_both_line_charts(i):
     
-    #correct an error where the last selected record becomes the first if the function used in succession 
-    if float(line_df.loc[0, 'date'].split('/')[1]) > float(line_df.loc[1, 'date'].split('/')[1]):
-        line_df = line_df.drop(0, axis=0)
+    url = 'https://qianxi.baidu.com/'
     
-    line_df.reset_index(inplace=True)
+    city_xpath = city_name_xpaths[i]
+           
+    #initialize web driver
+    driver = webdriver.Chrome(executable_path = executable_path)
     
-    line_df = line_df.filter(['date', 'value'])
+    #let page load
+    driver.get(url)
+    short_wait()
     
-    return(line_df)
+    #define buttons that are constant no matter the layout of the site
+    city_name_drop_down = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[1]/div/div')
+    incoming_button = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[3]/div/div[1]')
+    outgoing_button = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[3]/div/div[2]')
+
+    
+    #click on city dropdown
+    city_name_drop_down.click()
+    short_wait()
+        
+    try:
+        city_button = driver.find_element_by_xpath(city_xpath)
+        city_button.click()
+        short_wait()
+        
+        #make a directory with the city name
+        current_download_directory = download_directory_all + city_names[i]
+        os.mkdir(current_download_directory)
+        
+        outgoing_button.click()
+        
+        #line charts do not change for different date values
+        #scrape outgoing line chart
+        line_df = scrape_line_graph(driver)
+        write_csv(line_df, current_download_directory + '/' + city_names[i] + '_line_outgoing.csv')
+        
+        incoming_button.click()
+        short_wait()
+        #scrape incoming line chart
+        line_df = scrape_line_graph(driver)
+        write_csv(line_df, current_download_directory + '/' + city_names[i] + '_line_incoming.csv')
+        
+        short_wait()
+        
+        driver.quit()
+
+    except Exception as e: 
+        print(e)
+        
+        driver.quit()        
+    
+def write_csv(df, file_name):
+    if df is not None:
+        df.to_csv(file_name)
 #%%
 '''
 Initial website parse to extract static elements
@@ -164,18 +261,93 @@ date_names_sub = [i.replace('-', '_') for i in date_names]
 '''
 Using data from initial scrape, conduct full, systematic web scrape
 
+Proposed changes to workflow 
+    - scrape every line chart for every city (return and scrape every panel for every date)
+    - continue with headless driver
+    - run multiple windows in parallel (10) - while
+    
+    - avoid tru multiprocessing by batching into 10 RAM is not the issue but time.sleep()
+
 '''
 
-#i = 12
-
 download_directory_all = '/Users/hamishgibbs/Documents/nCOV-2019/Web_Scraping/Scraped_Data/'
+
+open_chrome_windows = 0
+
+for i in range(0, len(city_name_xpaths) + 1, 1):
+    scrape_both_line_charts(i)
+#%%
+with Pool(5) as p:
+    p.map(scrape_both_line_charts, list(range(0, len(city_name_xpaths) + 1, 1)))
+#%%
+    city_xpath = city_name_xpaths[i]
+           
+    #initialize web driver
+    driver = webdriver.Chrome(executable_path = executable_path)
+    
+    #let page load
+    driver.get(url)
+    short_wait()
+    
+    #define buttons that are constant no matter the layout of the site
+    city_name_drop_down = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[1]/div/div')
+    date_drop_down = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[2]/div/div/div')
+    incoming_button = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[3]/div/div[1]')
+    outgoing_button = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[3]/div/div[2]')
+
+    
+    #click on city dropdown
+    city_name_drop_down.click()
+    short_wait()
+        
+    try:
+        city_button = driver.find_element_by_xpath(city_xpath)
+        city_button.click()
+        short_wait()
+        
+        #get constant panel buttons
+        city_panel_button = driver.find_element_by_xpath('//*[@id="content"]/div/div[5]/div[1]/div/div[1]')
+        province_panel_button = driver.find_element_by_xpath('//*[@id="content"]/div/div[5]/div[1]/div/div[2]')
+        
+        #make a directory with the city name
+        current_download_directory = download_directory_all + city_names[i]
+        os.mkdir(current_download_directory)
+        
+        #line charts do not change for different date values
+        #scrape outgoing line chart
+        line_df = scrape_line_graph(driver)
+        write_csv(line_df, current_download_directory + '/' + city_names[i] + '_line_outgoing.csv')
+        
+        incoming_button.click()
+        short_wait()
+        #scrape incoming line chart
+        line_df = scrape_line_graph(driver)
+        write_csv(line_df, current_download_directory + '/' + city_names[i] + '_line_incoming.csv')
+        
+        #click on outgoing
+        outgoing_button.click()
+        short_wait()
+        
+        driver.quit()
+
+    except Exception as e: 
+        print(e)
+        
+        driver.quit()
+        
+        continue
+    
+
+    
+    
+#%%
 
 #here: sort out error handling to ensure that every city name is clicked
 #for every city hyperlink:
 
 error_occurred_last_time = False
 
-for i in range(12, len(city_name_xpaths) + 1, 1):
+for i in range(71, len(city_name_xpaths) + 1, 1):
     
     city_xpath = city_name_xpaths[i]
     
@@ -292,11 +464,51 @@ for i in range(12, len(city_name_xpaths) + 1, 1):
             
     except Exception as e: 
         print(e)
-        if i > 1:
-            break
-        
+
         error_occurred_last_time = True
         continue
 
     driver.quit()
 
+        
+#%%
+#update line scrape
+city_xpath = city_name_xpaths[71]
+
+#initialize web driver
+driver = webdriver.Chrome(executable_path = executable_path)
+
+#let page load
+driver.get(url)
+short_wait()
+
+#define buttons that are constant no matter the layout of the site
+city_name_drop_down = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[1]/div/div')
+date_drop_down = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[2]/div/div/div')
+outgoing_button = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[3]/div/div[1]')
+incoming_button = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[3]/div/div[2]')
+
+#click on city dropdown
+city_name_drop_down.click()
+short_wait()    
+
+city_button = driver.find_element_by_xpath(city_xpath)
+city_button.click()
+short_wait()
+
+#get constant panel buttons
+city_panel_button = driver.find_element_by_xpath('//*[@id="content"]/div/div[5]/div[1]/div/div[1]')
+province_panel_button = driver.find_element_by_xpath('//*[@id="content"]/div/div[5]/div[1]/div/div[2]')
+
+line_df = scrape_line_graph(driver)
+
+print(line_df)
+
+driver.quit()
+
+#when scrape is completed - return to scrape 202 line charts
+
+#this will get messed up with leap year
+#list of dates between today and 1/1/2020
+
+    
